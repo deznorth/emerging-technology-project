c57248b3f5da32d5872e0a651274514dce8c25d1
David Rojas <deznorth@gmail.com>
2022-12-01 18:32:46 -0500

    Add pocketbase and font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
+import Head from 'next/head'
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({ subsets: ['latin'] })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <div className={inter.className}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
 export default MyApp
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import PocketBase, { Record } from 'pocketbase';
 
-type Data = {
-  name: string
-}
+// DB Client
+const pb = new PocketBase(process.env.API_BASE_URL);
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Record[]>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const list = await pb.collection('users').getFullList();
+  res.status(200).json(list);
 }
